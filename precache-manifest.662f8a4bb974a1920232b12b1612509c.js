1750974f45a6b768191278ce64806413c68cc0af
Gokul Agiwal <gokulagiwal2287@gmail.com>
2019-06-19 22:42:47 +0100

    Updates

---
+++ precache-manifest.662f8a4bb974a1920232b12b1612509c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7732b7bc883d05201c9149358ad0d7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4c994d2b2148202f1bc6",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4c994d2b2148202f1bc6",
+    "url": "/portfolio/static/js/main.244beb62.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
